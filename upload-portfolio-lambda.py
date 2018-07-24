417508a8ed71b8184c0f05bebef80c507f2a1d0e
Robert Woods <robertwoods2013@gmail.com>
2018-07-24 14:46:43 -0400

    notification for build deploy

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,18 +2,29 @@ import boto3
 import StringIO
 import zipfile
 import mimetypes
+def lambda_handler(event, context):
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.robertwoods.info')
-build_bucket = s3.Bucket('portfoliobuild.robertwoods.info')
-portfolio_zip = StringIO.StringIO()
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:265051230937:deployPortfolioTopic')
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-#check what StringIO implements later
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.robertwoods.info')
+        build_bucket = s3.Bucket('portfoliobuild.robertwoods.info')
+        portfolio_zip = StringIO.StringIO()
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        #check what StringIO implements later
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject='Portfolio deployed', Message='Portfolio deployed succsesfully')
+    except:
+        topic.publish(Subject='Portfolio deployed', Message='Portfolio deployed unsuccsesfully')
+        raise
+    return ''
